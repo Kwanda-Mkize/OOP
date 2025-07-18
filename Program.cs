e6cee3a1a0a389b16aae78160b10d079038bb4ec
Kwanda-Mkize <kwanda.mkize@exazeit.com>
2025-07-18 16:03:42 +0200

    removed-singleton-files

--- Program.cs
+++ Program.cs
@@ -1,23 +1,7 @@
-﻿using OOP;
-using OPP;
-using System;
+﻿using System;
 using System.Threading;
 
-
-void clientAction(Adapter adapter)
-{
-  adapter.PerfomActivity();
-}
-
-// clientAction(new Client_1());
-clientAction(new Client_2());
-
-Logger.Instance.PrintWithdrawLogs();
-// Logger.Instance.PrintCardPurchaseLogs();
-// Logger.Instance.PrintDepositLogs();
-
-
-
-// new B();
+//Understaing the base class
+new B();
 
 
